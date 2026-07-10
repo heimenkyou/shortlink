68692ff7599d1c2f47292a9dcb910080a6d39136
heimenkyou <wenbin.lo@outlook.com>
2026-07-11 03:09:26 +0800

    fix: 修复导包路径

--- apps/backend/admin/src/main/java/cn/luowb/shortlink/admin/service/impl/UserServiceImpl.java
+++ apps/backend/admin/src/main/java/cn/luowb/shortlink/admin/service/impl/UserServiceImpl.java
@@ -4,8 +4,6 @@ import cn.dev33.satoken.stp.StpUtil;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.crypto.digest.BCrypt;
 import cn.luowb.shortlink.admin.common.biz.user.UserContext;
-import cn.luowb.shortlink.common.convention.exception.exception.ServiceException;
-import cn.luowb.shortlink.common.convention.exception.exception.ClientException;
 import cn.luowb.shortlink.admin.dao.entity.UserDO;
 import cn.luowb.shortlink.admin.dao.mapper.UserMapper;
 import cn.luowb.shortlink.admin.dto.req.UserLoginReqDTO;
@@ -14,6 +12,8 @@ import cn.luowb.shortlink.admin.dto.req.UserUpdateReqDTO;
 import cn.luowb.shortlink.admin.dto.resp.UserLoginRespDTO;
 import cn.luowb.shortlink.admin.dto.resp.UserRespDTO;
 import cn.luowb.shortlink.admin.service.UserService;
+import cn.luowb.shortlink.common.convention.ServiceException;
+import cn.luowb.shortlink.common.convention.exception.ClientException;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -24,8 +24,8 @@ import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
 
 import static cn.luowb.shortlink.admin.common.constant.RedisCacheKeyEnum.LOCK_USER_REGISTER_KEY;
-import static cn.luowb.shortlink.common.convention.exception.result.errorcode.BaseErrorCode.USER_NAME_EXIST_ERROR;
-import static cn.luowb.shortlink.common.convention.exception.result.errorcode.BaseErrorCode.USER_REGISTER_ERROR;
+import static cn.luowb.shortlink.common.convention.result.errorcode.BaseErrorCode.USER_NAME_EXIST_ERROR;
+import static cn.luowb.shortlink.common.convention.result.errorcode.BaseErrorCode.USER_REGISTER_ERROR;
 
 /**
  * 用户服务实现
