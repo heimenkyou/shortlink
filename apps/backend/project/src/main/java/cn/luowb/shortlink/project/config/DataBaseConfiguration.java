8098b954da9ea2425ff6cfb6d4e3aeebd534305b
heimenkyou <wenbin.lo@outlook.com>
2026-07-10 21:23:03 +0800

    feat(backend): 添加数据库字段自动填充配置类

---
+++ apps/backend/project/src/main/java/cn/luowb/shortlink/project/config/DataBaseConfiguration.java
@@ -0,0 +1,54 @@
+package cn.luowb.shortlink.project.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
+
+/**
+ * 数据库持久层配置类｜配置 MyBatis-Plus 相关分页插件等
+ */
+@Configuration
+public class DataBaseConfiguration {
+
+    /**
+     * MyBatis-Plus MySQL 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+    /**
+     * MyBatis-Plus 源数据自动填充类
+     */
+    @Bean
+    public MyMetaObjectHandler myMetaObjectHandler() {
+        return new MyMetaObjectHandler();
+    }
+
+    /**
+     * MyBatis-Plus 源数据自动填充类
+     */
+    public static class MyMetaObjectHandler implements MetaObjectHandler {
+
+        @Override
+        public void insertFill(MetaObject metaObject) {
+            strictInsertFill(metaObject, "createTime", LocalDateTime::now, LocalDateTime.class);
+            strictInsertFill(metaObject, "updateTime", LocalDateTime::now, LocalDateTime.class);
+            strictInsertFill(metaObject, "delFlag", () -> 0, Integer.class);
+        }
+
+        @Override
+        public void updateFill(MetaObject metaObject) {
+            strictUpdateFill(metaObject, "updateTime", LocalDateTime::now, LocalDateTime.class);
+        }
+    }
+}
