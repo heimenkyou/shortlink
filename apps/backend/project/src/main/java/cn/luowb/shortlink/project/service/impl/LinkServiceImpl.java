2cb03ec83d0ec8cbe919be86a94d0b2f98b76861
heimenkyou <wenbin.lo@outlook.com>
2026-07-10 21:23:49 +0800

    optimize(backend): 短链接生成增加重试机制，使用布隆过滤器判断是否存在

--- apps/backend/project/src/main/java/cn/luowb/shortlink/project/service/impl/LinkServiceImpl.java
+++ apps/backend/project/src/main/java/cn/luowb/shortlink/project/service/impl/LinkServiceImpl.java
@@ -1,6 +1,8 @@
 package cn.luowb.shortlink.project.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.IdUtil;
+import cn.luowb.shortlink.common.convention.ServiceException;
 import cn.luowb.shortlink.project.dao.entity.LinkDO;
 import cn.luowb.shortlink.project.dao.mapper.LinkMapper;
 import cn.luowb.shortlink.project.dto.req.LinkCreateReqDTO;
@@ -8,22 +10,36 @@ import cn.luowb.shortlink.project.dto.resp.LinkCreateRespDTO;
 import cn.luowb.shortlink.project.service.LinkService;
 import cn.luowb.shortlink.project.util.HashUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RBloomFilter;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
 /**
  * 短链接服务实现类
  */
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class LinkServiceImpl extends ServiceImpl<LinkMapper, LinkDO> implements LinkService {
+    private final RBloomFilter<String> shortUriCreateCachePenetrationBloomFilter;
 
     @Override
     public LinkCreateRespDTO createShortLink(LinkCreateReqDTO requestParam) {
         String suffix = generateSuffix(requestParam);
+        String fullShortUrl = requestParam.getDomain() + "/" + suffix;
         LinkDO linkDO = BeanUtil.toBean(requestParam, LinkDO.class);
-        linkDO.setFullShortUrl(requestParam.getDomain() + "/" + suffix);
+        linkDO.setFullShortUrl(fullShortUrl);
         linkDO.setShortUri(suffix);
         linkDO.setEnableStatus(0);
-        this.save(linkDO);
+        try {
+            this.save(linkDO);
+        } catch (DuplicateKeyException e) {
+            log.warn("短链接重复入库，{}", suffix);
+            throw new ServiceException("短链接生成冲突，请稍后重试");
+        }
+        shortUriCreateCachePenetrationBloomFilter.add(fullShortUrl);
         return BeanUtil.toBean(linkDO, LinkCreateRespDTO.class);
     }
 
@@ -33,7 +49,16 @@ public class LinkServiceImpl extends ServiceImpl<LinkMapper, LinkDO> implements 
      * @return 短链接后缀
      */
     private String generateSuffix(LinkCreateReqDTO requestParam) {
-        String originUrl = requestParam.getOriginUrl();
-        return HashUtil.hashToBase62(originUrl);
+        int generateCount = 0;
+        String shortUri;
+        do {
+            if (generateCount > 10) {
+                throw new ServiceException("短链接生成频繁，请稍后重试");
+            }
+            String originUrl = requestParam.getOriginUrl();
+            shortUri = HashUtil.hashToBase62(originUrl + IdUtil.fastSimpleUUID());
+            generateCount++;
+        } while (shortUriCreateCachePenetrationBloomFilter.contains(requestParam.getDomain() + "/" + shortUri));
+        return shortUri;
     }
 }
