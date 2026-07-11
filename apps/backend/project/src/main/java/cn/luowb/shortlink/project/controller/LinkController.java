48115538b59fa82c1174be4053a656f27a2c149f
heimenkyou <wenbin.lo@outlook.com>
2026-07-11 16:31:07 +0800

    fix(backend): 短链接请求方式

--- apps/backend/project/src/main/java/cn/luowb/shortlink/project/controller/LinkController.java
+++ apps/backend/project/src/main/java/cn/luowb/shortlink/project/controller/LinkController.java
@@ -11,6 +11,7 @@ import cn.luowb.shortlink.project.service.LinkService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,8 +38,8 @@ public class LinkController {
      * 分页查询短链接
      */
     @Operation(summary = "分页查询短链接")
-    @PostMapping("/api/short-link/v1/page")
-    public Result<PageResult<LinkPageRespDTO>> pageShortLink(@RequestBody LinkPageReqDTO requestParam) {
+    @GetMapping("/api/short-link/v1/page")
+    public Result<PageResult<LinkPageRespDTO>> pageShortLink(LinkPageReqDTO requestParam) {
         return Results.success(linkService.pageShortLink(requestParam));
     }
 }
--- apps/backend/project/src/main/java/cn/luowb/shortlink/project/dto/req/LinkPageReqDTO.java
+++ apps/backend/project/src/main/java/cn/luowb/shortlink/project/dto/req/LinkPageReqDTO.java
@@ -2,6 +2,7 @@ package cn.luowb.shortlink.project.dto.req;
 
 import cn.luowb.shortlink.common.dto.BasePageQuery;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
@@ -11,5 +12,6 @@ public class LinkPageReqDTO extends BasePageQuery {
      * 分组标识
      */
     @Schema(description = "分组标识")
+    @NotBlank(message = "分组标识不能为空")
     private String gid;
 }
